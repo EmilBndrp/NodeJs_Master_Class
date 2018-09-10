e94495d955b3b9d4dc5bee6383fee9e2540776bf
dashed92 <dashed92@gmail.com>
2018-09-10 16:14:29 +0200

    Implemented handlers
    getting the requested path and choose the appropriate handler

---
+++ Assignment-1/handlers.js
@@ -0,0 +1,32 @@
+/**
+ * Handlers
+ */
+const handlers = {};
+
+// Ping handler
+handlers.ping = function(callback) {
+    callback(200);
+}
+
+// hello handler
+handlers.hello = function(callback) {
+    // Callback a HTTP status code and a payload object
+    callback(200, {'name': 'samplehandler'});
+};
+
+// Not found handler
+handlers.notFound = function(callback) {
+    callback(404);
+};
+
+
+/**
+ * Routing
+ */
+const router = {
+    'ping': handlers.ping,
+    'hello': handlers.hello,
+    'notFound': handlers.notFound
+};
+
+module.exports = router;
--- Assignment-1/index.js
+++ Assignment-1/index.js
@@ -2,17 +2,71 @@
  * Base file
  */
 
-// Dependencies
+ 
+/**
+ * Dependencies and variables
+ */
+// NodeJs dependencies
+const url = require('url');
 const http = require('http');
 
-let port = 3000;
+// Local dependencies
+const router = require('./handlers');
 
-// Function decleration
+// Variables
+let port = 80;
+
+
+/**
+ * Function decleration
+ */
 const server = http.createServer((req, res) => {
-    res.end('hello');
+
+    // Getting the path
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Getting the method
+    const method = req.method.toLowerCase();
+
+
+    // Setting up appropriate handler
+    let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ?
+    router[trimmedPath] : router['notFound'];
+
+    chosenHandler((statusCode, payload) => {
+
+        // If current status code is invalid, set it to 200
+        statusCode = typeof(statusCode) == 'number' ? 
+        statusCode : 200;
+
+        // If current payload is invalid set it to empty object
+        payload = typeof(payload) == 'object' ? 
+        payload : {};
+
+        // Stringify payload
+        let payloadString = JSON.stringify(payload);
+        
+
+        // Assign the content type to JSON object
+        res.setHeader('Content-Type', 'application/json');
+
+        // send status code and payload to client
+        res.writeHead(statusCode)
+        res.end(payloadString);
+
+        // log The requested data
+        console.log('------------------- New Request ----------------------' + '\n' +
+                    '- Request recieved on path: ' + trimmedPath + '\n' +
+                    '- with this method: ' + method + '\n'
+                    );
+    });
 });
 
-// Function invocation
+/**
+ * Function invocation
+ */
 server.listen(port,() => {
     console.log('The server is open on port '+ port);
     

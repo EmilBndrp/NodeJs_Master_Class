61c14cc5487ebfee9b12407652ad662f5f6b45ed
dashed92 <dashed92@gmail.com>
2018-09-17 13:59:17 +0200

    Added users.put (doedn't seem to work on windows)

--- Section 3/RESTfulApp/lib/handlers.js
+++ Section 3/RESTfulApp/lib/handlers.js
@@ -114,8 +114,62 @@ handlers._users.get = function(data, callback) {
 };
 
 // Users - put
+// required data: phone
+// Optional data: firstname, lastname, password (at least one must be specified)
 handlers._users.put = function(data, callback) {
+    
+    const firstName = typeof(data.payload.firstName) == 'string' && 
+        data.payload.firstName.trim().length > 0 ? 
+        data.payload.firstName.trim() : false;
 
+    const lastName = typeof(data.payload.lastName) == 'string' && 
+        data.payload.lastName.trim().length > 0 ? 
+        data.payload.lastName.trim() : false;
+
+    const phone = typeof(data.payload.phone) == 'string' && 
+        data.payload.phone.trim().length == 10 ? 
+        data.payload.phone.trim() : false;
+
+    const password = typeof(data.payload.password) == 'string' && 
+        data.payload.password.trim().length > 0 ? 
+        data.payload.password.trim() : false;
+
+    //Error if the phone is invalid
+    if (phone) {
+
+    } else {
+        callback(400, { 'Error': 'Missing required field (phone Nr. is invalid)'})
+        if ( firstName || lastName || password ) {
+            _data.read('users', phone, (err, userData) => {
+                if (!err && userData) {
+                    // Update the fileds that are neccesary
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.hashedPassword = helpers.hash(password);
+                    }
+
+                    // Store the new updates
+                    _data.update('users', phone, userData, (err) => {
+                        if (!err) {
+                            callback(200);
+                        } else {
+                            callback(500, { 'Error': 'Could not update the user'});
+                        }
+                    })
+                } else {
+                    callback(400, { 'Error': 'The specified user does not exist'});
+                }
+            })
+
+        } else {
+            callback(400, { 'Error': 'Missing fields to update'});
+        }
+    }
 };
 
 // Users - delete

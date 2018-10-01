cdf7a3e43e4c815b64b8463c4547083cd68e3e3c
EmilBNDRP <emil.bndrp@gmail.com>
2018-10-01 13:15:32 +0200

    changed lint rule (space-in-parens)

--- Section 3/RESTfulApp/lib/config.js
+++ Section 3/RESTfulApp/lib/config.js
@@ -41,12 +41,12 @@ environments.production = {
 };
 
 // Determine which environment was passed as a command-line argument
-const currentEnvironment = typeof ( process.env.NODE_ENV ) === 'string' ?
+const currentEnvironment = typeof (process.env.NODE_ENV) === 'string' ?
     process.env.NODE_ENV.toLowerCase() :
     '';
 
 // Check that the current environment is one of the environments above, if no, default to staging
-const environmentToExport = typeof ( environments[currentEnvironment] ) === 'object' ?
+const environmentToExport = typeof (environments[currentEnvironment]) === 'object' ?
     environments[currentEnvironment] :
     environments.staging;
 
--- Section 3/RESTfulApp/lib/helpers.js
+++ Section 3/RESTfulApp/lib/helpers.js
@@ -3,18 +3,18 @@
  */
 
 // Dependencies
-const crypto = require( 'crypto' );
-const config = require( './config' );
+const crypto = require('crypto');
+const config = require('./config');
 
 // Container for all the helpers
 const helpers = {};
 
 // Create a SHA256 hash from string
-helpers.hash = function ( str ) {
-    if ( typeof ( str ) === 'string' && str.length > 0 ) {
-        const hash = crypto.createHmac( 'sha256', config.hashingSecret )
-            .update( str )
-            .digest( 'hex' );
+helpers.hash = function (str) {
+    if (typeof (str) === 'string' && str.length > 0) {
+        const hash = crypto.createHmac('sha256', config.hashingSecret)
+            .update(str)
+            .digest('hex');
 
         return hash;
     }
@@ -23,31 +23,31 @@ helpers.hash = function ( str ) {
 };
 
 // Parse a JSON string to an object in all cases, without throwing an error
-helpers.parseJsonToObject = function ( str ) {
+helpers.parseJsonToObject = function (str) {
     try {
-        const obj = JSON.parse( str );
+        const obj = JSON.parse(str);
 
         return obj;
-    } catch ( err ) {
+    } catch (err) {
         return {};
     }
 };
 
-helpers.createRandomString = function ( strLength ) {
-    strLength = typeof ( strLength ) === 'number' && strLength > 0 ?
+helpers.createRandomString = function (strLength) {
+    strLength = typeof (strLength) === 'number' && strLength > 0 ?
         strLength :
         false;
 
-    if ( strLength ) {
+    if (strLength) {
         // Define all the possible characters that could go int a string
         const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
         // Start the final string
         let str = '';
 
-        for ( let i = 1; i <= strLength; i++ ) {
+        for (let i = 1; i <= strLength; i++) {
             // Get a random character from the possibleCharacters string
-            const randomCharacter = possibleCharacters.charAt( Math.floor( Math.random() * possibleCharacters.length ));
+            const randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
 
             // Append this character to the final string
             str += randomCharacter;

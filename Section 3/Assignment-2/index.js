56ef994df8da8eddcb5e4e3bf9dc9922b1246923
EmilBndrp <emil.bndrp@gmail.com>
2018-10-17 16:00:57 +0200

    Starting assignment 2

---
+++ Section 3/Assignment-2/index.js
@@ -0,0 +1,21 @@
+// Dependendies
+const server = require('./lib/server');
+const workers = require('./lib/workers');
+
+// Declare the app
+const app = {};
+
+// Export the app
+module.exports = app;
+
+// Init function
+app.init = function initiation() {
+  // Start the server
+  server.init();
+
+  // Start the workers
+  workers.init();
+};
+
+// Execute the app
+app.init();
